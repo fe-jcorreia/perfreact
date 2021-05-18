5c384dc110acc4d1ba5a30581568085c0aaf83b8
fernando <fe.jcorreia@gmail.com>
2021-05-18 10:26:00 -0300

    virtualization added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "127a75a615698e4c0d28"; }
+/******/ 		__webpack_require__.h = function() { return "223ee67bcdb170621fc2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from "react";
+// import { useMemo } from "react";
 import { ProductItem } from "./ProductItem";
+import { List, ListRowRenderer } from "react-virtualized";
 
 interface SearchResultProps {
   totalPrice: string;
@@ -17,18 +18,29 @@ export function SearchResult({
   results,
   onAddToWishList,
 }: SearchResultProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          onAddToWishList={onAddToWishList}
+          product={results[index]}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map((product) => {
-        return (
-          <ProductItem
-            key={product.id}
-            onAddToWishList={onAddToWishList}
-            product={product}
-          />
-        );
-      })}
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
